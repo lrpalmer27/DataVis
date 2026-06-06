db073858ffe58401a38df97e372bf33536dc3020
lrpalmer27 <logan@palmers.ca>
2026-06-06 13:35:15 -0700

    post processing is working

--- get-data.py
+++ get-data.py
@@ -2,6 +2,8 @@ import os
 import datetime
 import wget 
 import zipfile
+import pandas as pd
+from datetime import datetime, timezone
 
 """
 The purpose of this file is to retrieve data from binance programmatically, save it to a location, and post process it, given a date range.
@@ -9,13 +11,14 @@ Browser compatible data exploration:
     https://www.binance.us/finder?dpath=public_data%2Fspot%2Fdaily%2Fklines%2FBTCUSD%2F1d
     
 """
-def aquireData(daterange):
+def aquireData(basesavepath, daterange, verbose=0):
     """
     The objective of this function is to download all the data within the given daterange and unpack the zip files. 
-    Parameters are daterange(start,end), both bounds inclusive
+    Parameters are:
+        basesave path (str) - folder location, does not have to exist
+        daterange(start,end) - both bounds inclusive
+        verbose - false by default.
     """
-
-    basesavepath = './.rawdata'
     
     #make sure subfolders exist that are needed: 
     os.makedirs(basesavepath, exist_ok=True) #make basepath if not existing
@@ -50,18 +53,47 @@ def aquireData(daterange):
     fullstring=[f"{basesavepath}/zipfiles/{n}" for n in zippedfiles]
     [os.remove(n) for n in fullstring]
 
-def postprocessing():
-    None
+def postprocessing(basesavepath, verbose=0):
+    """
+    The objective of this function is to grab all data generated above, and parse csv files into one file for ease of future use.
+    Parameters are:
+        basesavepath (str) - folder location, does not have to exist
+        daterange(start,end) - both bounds inclusive
+        verbose - false by default.
+    """
+    listcsvfiles = [f"{basesavepath}/csvfiles/{n}" for n in os.listdir(f"{basesavepath}/csvfiles")]
+    
+    df = pd.concat((pd.read_csv(f) for f in listcsvfiles), ignore_index=True)
+    
+   
+    start=datetime.fromtimestamp(df.iloc[0,df.columns.get_loc('open_time')]/1000,tz=timezone.utc).strftime('%Y-%m-%d')
+    end=datetime.fromtimestamp(df.iloc[-1,df.columns.get_loc('close_time')]/1000,tz=timezone.utc).strftime('%Y-%m-%d')
+    
+    df.to_pickle(f"{basesavepath}/BTCUSD_{start}_{end}.pkl")
+    
+    if verbose: 
+        print(listcsvfiles) #check that csv file names are generated correctly.
+        print(df.head(5)) #check df data
+        print('Open date: ', start, "close date: ", end) #checking start and end date of data
+        
+    # delete csv folder content for storage saving
+    [os.remove(n) for n in listcsvfiles]
     
 
 if __name__ == "__main__": 
     verbose = 1 
-    performfcn = 'aquiredata' #aquiredata, or postprocess
+    basesavepath = './.rawdata'
+    
+    # performfcn = 'aquiredata' #aquiredata, or postprocess
+    performfcn = 'postprocess' #aquiredata, or postprocess
     
     if performfcn == 'aquiredata': 
         #update date range as desired
-        startdate = datetime.date(2019,9,17)
-        enddate = datetime.date(2019,9,20) #datetime.datetime.now().strftime('%Y-%m-%d')-1
+        startdate = datetime.date(2026,6,1) #2026,9,17 is the first day of data in binance
+        enddate = datetime.date(2026,6,5) #datetime.datetime.now().strftime('%Y-%m-%d')-1
         
-        aquireData(daterange=(startdate, enddate))
+        aquireData(basesavepath,daterange=(startdate, enddate),verbose=verbose)
+    
+    elif performfcn == 'postprocess': 
+        postprocessing(basesavepath,verbose=verbose)
     
